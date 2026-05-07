2f5f73b4b2f71ab864e6d06f9b595927f0c4138b
TRILLAT Germain <dgerms@gmail.com>
2026-05-07 02:01:16 +0200

    fix: add allowOverwrite: true to Blob put() so admin saves work

--- api/site-data.js
+++ api/site-data.js
@@ -50,6 +50,7 @@ module.exports = async function handler(req, res) {
         access: 'public',
         contentType: 'application/json',
         addRandomSuffix: false,
+        allowOverwrite: true,
         token: process.env.BLOB_READ_WRITE_TOKEN,
       });
 
